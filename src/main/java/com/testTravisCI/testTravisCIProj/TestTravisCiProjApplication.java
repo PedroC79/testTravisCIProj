0926368eaada78a55bbe6a4934fa63c560a8f808
Pedro Carvalho <pedro.speedy79@gmail.com>
2018-02-07 22:07:57 +0000

    Initial Setup

---
+++ src/main/java/com/testTravisCI/testTravisCIProj/TestTravisCiProjApplication.java
@@ -0,0 +1,12 @@
+package com.testTravisCI.testTravisCIProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTravisCiProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTravisCiProjApplication.class, args);
+	}
+}
